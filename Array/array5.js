a9f0a57606028936ec065ce6c646e6adc69525e0
Satva39 <satvachauhan11@gmail.com>
2025-05-18 11:02:48 +0530

    new file:   Array/array5.js

---
+++ Array/array5.js
@@ -0,0 +1,14 @@
+// var language = ["hindi","english","french","gujarati","marathi","urdu"]
+// var x = language.filter((lang)=>{
+    // return lang != "marathi"
+// })
+// console.log(x)
+// 
+// const deletelang = (langName)=> language.filter((lang)=> lang != langName)
+// let ans = deletelang("english")
+
+var language = ["hindi","english"]
+language.forEach((lan)=>{
+    console.log(lan);
+    
+})
